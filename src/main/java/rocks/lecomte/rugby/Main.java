b5e81e1641dc577d37e78e6daff6ae36990c2639
dave <davidlecomte@email.com>
2019-08-26 07:56:38 +0100

    Updated to ignore .idea directory

--- src/main/java/rocks/lecomte/rugby/Main.java
+++ src/main/java/rocks/lecomte/rugby/Main.java
@@ -3,6 +3,7 @@ package rocks.lecomte.rugby;
 import rocks.lecomte.rugby.domain.GameResult;
 import rocks.lecomte.rugby.impl.PickAndGoResultObtainer;
 
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -18,14 +19,15 @@ public class Main {
         Map<String, List<GameResult>> teama = results.stream().collect(groupingBy(GameResult::getTeamA));
         Map<String, List<GameResult>> teamb = results.stream().collect(groupingBy(GameResult::getTeamB));
 
+        List<String>totalPoints = new LinkedList<>();
         for (String team : teama.keySet()) {
-            System.out.println(team);
-            int points = teama.get(team).stream().map(r -> Integer.valueOf(r.getPointsA())).collect(Collectors.summingInt(Integer::intValue));
+            int points = teama.get(team).stream().mapToInt(r -> Integer.valueOf(r.getPointsA())).sum();
             List<GameResult>playedAsB = teamb.get(team);
             if (playedAsB != null) {
-                points += playedAsB.stream().map(r -> Integer.valueOf(r.getPointsB())).collect(Collectors.summingInt(Integer::intValue));
+                points += playedAsB.stream().mapToInt(r -> Integer.valueOf(r.getPointsB())).sum();
             }
-            System.out.println("Points " + points);
+            totalPoints.add(points + " " + team);
         }
+        totalPoints.stream().sorted((a,b) -> new Integer(a.split(" ")[0]).compareTo(Integer.parseInt(b.split(" ")[0]))).forEach(System.out::println);
     }
 }
